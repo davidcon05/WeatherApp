7208fbec45fd04e7d7598109df789a808a907ef4
David Contreras <david.b.contreras@gmail.com>
2023-04-28 08:41:32 -0400

    update to add background contrast for feature elements

--- app/src/main/java/com/plcoding/weatherapp/presentation/WeatherForecast.kt
+++ app/src/main/java/com/plcoding/weatherapp/presentation/WeatherForecast.kt
@@ -3,42 +3,52 @@ package com.plcoding.weatherapp.presentation
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.plcoding.weatherapp.presentation.ui.theme.DarkBlue
 
 @Composable
 fun WeatherForecast(
     state: WeatherState,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    backgroundColor: Color = DarkBlue
 ) {
     state.weatherInfo?.weatherDataPerDay?.get(0)?.let { data ->
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 16.dp)
+        Card(
+            backgroundColor = backgroundColor,
+            shape = RoundedCornerShape(10.dp),
+            modifier = modifier.padding(16.dp)
         ) {
-            Text(
-                text = "Today",
-                fontSize = 16.sp,
-                color = Color.White
-            )
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+            ) {
+                Text(
+                    text = "Today",
+                    fontSize = 16.sp,
+                    color = Color.White
+                )
 
-            Spacer(modifier = Modifier.height(16.dp))
+                Spacer(modifier = Modifier.height(16.dp))
 
-            LazyRow(content = {
-                items(data)  { weatherData ->
-                    HourlyWeatherDisplay(
-                        weatherData = weatherData,
-                        modifier = Modifier
-                            .height(96.dp)
-                            .padding(horizontal = 16.dp)
-                    )
-                }
-            })
+                LazyRow(content = {
+                    items(data) { weatherData ->
+                        HourlyWeatherDisplay(
+                            weatherData = weatherData,
+                            modifier = Modifier
+                                .height(96.dp)
+                                .padding(horizontal = 16.dp)
+                        )
+                    }
+                })
+            }
         }
     }
 }
--- app/src/main/java/com/plcoding/weatherapp/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/plcoding/weatherapp/presentation/ui/theme/Color.kt
@@ -5,3 +5,4 @@ import androidx.compose.ui.graphics.Color
 val DarkBlue = Color(0xFF1B3B5A)
 val DeepBlue = Color(0xFF102840)
 val Stormy = Color(red = 120, green = 147, blue = 161)
+val OffWhite = Color(red = 244, green = 244, blue = 244)
